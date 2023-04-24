50366514d4c74f0280a612bde6a97da92d19d6d9
giorgio-villani <15069760+giorgio-villani@users.noreply.github.com>
2023-04-24 18:36:45 -0500

    added validatorform, profile and rolodex forms

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,41 @@
 import styles from "@/styles/Header.module.css";
 
-import { Box, Button, ButtonGroup, HStack, Link, Spacer, Stack, Text } from "@chakra-ui/react";
+import { Box, Button, ButtonGroup, HStack, Spacer, Stack, Text, Link } from "@chakra-ui/react";
 import { CustomConnectButton } from "./CustomConnectButton";
 
 const Header = () => {
   return (
     <header className={styles.navbar}>
-      <Link href='/'>
-        <HStack>
-          <img src='/logo.png' alt='Krypto Kredit logo' className={styles.logo} />
-          <Text fontSize='2xl' as='b'>
-            Krypto Kredit
-          </Text>
-        </HStack>
-      </Link>
-      <Spacer />
-      <ButtonGroup variant='ghost' colorScheme='gray'>
-        <Link href='/Invoices'>
-          <Button size='sm'>Invoices</Button>
+      <div className={styles.brand}>
+        <Link href='/'>
+          <HStack>
+            <img className={styles.logo} src='/logo.png' alt='Krypto Kredit logo' />
+            <Text className={styles.logoText}>Krypto Kredit</Text>
+          </HStack>
         </Link>
-        <Link href='/Validators'>
-          <Button size='sm'>Validators</Button>
+      </div>
+      <div className={styles.menu}>
+        <Link href='#' className='toggleButton'>
+          <span class='bar'></span>
+          <span class='bar'></span>
+          <span class='bar'></span>
         </Link>
-        <Link href='/Profiles'>
-          <Button size='sm'>Profile</Button>
-        </Link>
-        <CustomConnectButton />
-      </ButtonGroup>
+        <ButtonGroup variant='ghost' colorScheme='gray'>
+          <Link className='navbarLinks' href='/Invoices'>
+            <Button size='sm'>Invoices</Button>
+          </Link>
+          <Link className='navbarLinks' href='/Validators'>
+            <Button size='sm'>Validators</Button>
+          </Link>
+          <Link className='navbarLinks' href='/Profiles'>
+            <Button size='sm'>Profile</Button>
+          </Link>
+          <Link className='navbarLinks' href='/Rolodex'>
+            <Button size='sm'>Rolodex</Button>
+          </Link>
+          <CustomConnectButton className={styles.menu_item} />
+        </ButtonGroup>
+      </div>
     </header>
   );
 };
--- src/pages/Validators/index.jsx
+++ src/pages/Validators/index.jsx
@@ -14,6 +14,7 @@ import {
   Select,
   NumberInput,
   NumberInputField,
+  Divider,
 } from "@chakra-ui/react";
 
 const ValidatorForm = () => {
