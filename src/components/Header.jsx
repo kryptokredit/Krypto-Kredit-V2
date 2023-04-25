efe849242fd20c1fa54461569bc6796d8bf0fadd
Metachaser24 <elomukoro@gmail.com>
2023-04-25 16:52:47 -0500

    made main page responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,51 @@
 import styles from "@/styles/Header.module.css";
-
+import { useState } from 'react';
 import { Box, Button, ButtonGroup, HStack, Spacer, Stack, Text, Link } from "@chakra-ui/react";
 import { CustomConnectButton } from "./CustomConnectButton";
 
+ 
+
+const toggleMenu = () => {
+  setMenuVisible(!menuVisible);
+}
+
 const Header = () => {
+
+  const [menuVisible, setMenuVisible] = useState(false);
+  
   return (
     <header className={styles.navbar}>
-      <div className={styles.brand}>
-        <Link href='/'>
-          <HStack>
-            <img className={styles.logo} src='/logo.png' alt='Krypto Kredit logo' />
-            <Text className={styles.logoText}>Krypto Kredit</Text>
-          </HStack>
+    <div className={styles.brand}>
+      <Link href='/'>
+        <HStack>
+          <img className={styles.logo} src='/logo.png' alt='Krypto Kredit logo' />
+          <Text className={styles.logoText}>Krypto Kredit</Text>
+        </HStack>
+      </Link>
+    </div>
+    <div className={`${styles.menu} ${menuVisible ? styles.menuVisible : ''}`}>
+      <Link href='#' className='toggleButton' onClick={toggleMenu}>
+        <span class='bar'></span>
+        <span class='bar'></span>
+        <span class='bar'></span>
+      </Link>
+      <ButtonGroup variant='ghost' colorScheme='gray'>
+        <Link className='navbarLinks' href='/Invoices'>
+          <Button size='sm'>Invoices</Button>
         </Link>
-      </div>
-      <div className={styles.menu}>
-        <Link href='#' className='toggleButton'>
-          <span class='bar'></span>
-          <span class='bar'></span>
-          <span class='bar'></span>
+        <Link className='navbarLinks' href='/Validators'>
+          <Button size='sm'>Validators</Button>
         </Link>
-        <ButtonGroup variant='ghost' colorScheme='gray'>
-          <Link className='navbarLinks' href='/Invoices'>
-            <Button size='sm'>Invoices</Button>
-          </Link>
-          <Link className='navbarLinks' href='/Validators'>
-            <Button size='sm'>Validators</Button>
-          </Link>
-          <Link className='navbarLinks' href='/Profiles'>
-            <Button size='sm'>Profile</Button>
-          </Link>
-          <Link className='navbarLinks' href='/Rolodex'>
-            <Button size='sm'>Rolodex</Button>
-          </Link>
-          <CustomConnectButton className={styles.menu_item} />
-        </ButtonGroup>
-      </div>
-    </header>
+        <Link className='navbarLinks' href='/Profiles'>
+          <Button size='sm'>Profile</Button>
+        </Link>
+        <Link className='navbarLinks' href='/Rolodex'>
+          <Button size='sm'>Rolodex</Button>
+        </Link>
+        <CustomConnectButton className={styles.menu_item} />
+      </ButtonGroup>
+    </div>
+  </header>
   );
 };
 
--- src/pages/Invoices/index.jsx
+++ src/pages/Invoices/index.jsx
@@ -1,54 +1,45 @@
 import InvoicerTableAll from '@/components/InvoicerTableAll'
 import { Tabs, TabList, TabPanels, Tab, TabPanel, Box, Card, CardHeader, Heading, Button, Link, Flex, Spacer } from '@chakra-ui/react'
 
-
 const InvoiceTabs = () => {
-    return(
-  <div className='container'>
-     <Card margin={10} maxWidth={1000} variant='unstyled'>
+  return (
+    <div className='container'>
+      <Card margin={10} maxWidth={1000} variant='unstyled'>
         <CardHeader justifyContent='center'></CardHeader>
-<Heading size='md' textAlign='center'>
-            My Invoices
-          </Heading>
-         
-        
-<Tabs mt={7} width={1000} variant='enclosed'>
-  <TabList >
-    <Tab>Unpaid</Tab>
-    <Tab>Paid</Tab>
-    <Tab>Outstanding</Tab>
-    <Tab>All</Tab>
-      <Spacer />
-      <Link  href='/Invoices/InvoiceForm'>
-    <Button mb={1} size='md' colorScheme='whatsapp' variant='solid'>+ Create an Invoice</Button>
-    </Link>
-  </TabList>
-  <TabPanels>
-    <TabPanel>
-    <InvoicerTableAll />
-    </TabPanel>
-    <TabPanel>
-<InvoicerTableAll />
-    </TabPanel>
-    <TabPanel>
-<InvoicerTableAll />
-    </TabPanel>
-    <TabPanel>
-<InvoicerTableAll />
-    </TabPanel>
-  </TabPanels>
-</Tabs>
-</Card>
-      </div>
-
-
-
-
- 
-
-    )
-
-
+        <Heading size='md' textAlign='center'>
+          My Invoices
+        </Heading>
+        <Box mt={7} width={1000}  maxW={{base: '100%', md: '1000px'}} overflowX='auto'>
+          <Tabs variant='enclosed'>
+            <TabList overflowX='scroll' whiteSpace='nowrap' w={{base: '100%', md: 'auto'}}>
+              <Tab>Unpaid</Tab>
+              <Tab>Paid</Tab>
+              <Tab>Outstanding</Tab>
+              <Tab>All</Tab>
+              <Spacer />
+              <Link href='/Invoices/InvoiceForm'>
+                <Button mb={1} size='md' colorScheme='whatsapp' variant='solid'>+ Create an Invoice</Button>
+              </Link>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <InvoicerTableAll />
+              </TabPanel>
+              <TabPanel>
+                <InvoicerTableAll />
+              </TabPanel>
+              <TabPanel>
+                <InvoicerTableAll />
+              </TabPanel>
+              <TabPanel>
+                <InvoicerTableAll />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
+      </Card>
+    </div>
+  )
 }
 
 export default InvoiceTabs
