663874849c93ef55b2a7b1130acaf53c0944c854
Metachaser24 <Elomukoro@gmail.com>
2023-05-19 17:10:23 -0400

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,60 +14,65 @@ const Header = () => {
 
   return (
     <header className={styles.navbar}>
-    <div className={styles.brand}>
-      <Link href='/'>
-        <HStack>
-          <img className={styles.logo} src='/logo.png' alt='Krypto Kredit logo' />
-          <Text className={styles.logoText}>Krypto Kredit</Text>
-        </HStack>
+  <div className={styles.brand}>
+    <Link href='/'>
+      <HStack>
+        <img className={styles.logo} src='/logo.png' alt='Krypto Kredit logo' />
+        <Text className={styles.logoText}>Krypto Kredit</Text>
+      </HStack>
+    </Link>
+  </div>
+
+  {/* Collapsed button for small screens */}
+  <Box display={{ base: 'block', md: 'none' }} ml='auto'>
+    <Button onClick={handleToggleMenu} variant="outline">
+      <HamburgerIcon />
+    </Button>
+  </Box>
+
+  {/* Collapsed menu */}
+  <Box mt="12" display={{ base: isMenuOpen ? 'flex' : 'none', md: 'none' }} flexDirection="column">
+   
+   
+   
+    <Stack spacing={3} mt={20}>
+      <Link href='/Invoices'>
+        <Button size='sm' w="full">Invoices</Button>
       </Link>
-    </div>
+      <Link href='/Validators'>
+        <Button size='sm' w="full">Validators</Button>
+      </Link>
+      <Link href='/Profiles'>
+        <Button size='sm' w="full">Profile</Button>
+      </Link>
+      <Link href='/Rolodex'>
+        <Button size='sm' w="full">Rolodex</Button>
+      </Link>
+      <CustomConnectButton w="full" />
+    </Stack>
+  </Box>
 
-    {/* Collapsed button for small screens */}
-    <Box display={{ base: 'block', md: 'none' }} ml='auto'>
-      <Button onClick={handleToggleMenu} variant="outline">
-        <HamburgerIcon />
-      </Button>
-    </Box>
+ 
 
-    {/* Collapsed menu */}
-    <Box mt="12" display={{ base: isMenuOpen ? 'flex' : 'none', md: 'none' }} flexDirection="column">
-      <Stack spacing={3} mt={20}>
-        <Link href='/Invoices'>
-          <Button size='sm' w="full">Invoices</Button>
-        </Link>
-        <Link href='/Validators'>
-          <Button size='sm' w="full">Validators</Button>
-        </Link>
-        <Link href='/Profiles'>
-          <Button size='sm' w="full">Profile</Button>
-        </Link>
-        <Link href='/Rolodex'>
-          <Button size='sm' w="full">Rolodex</Button>
-        </Link>
-        <CustomConnectButton w="full" />
-      </Stack>
-    </Box>
-
-    {/* Expanded menu for large screens */}
-    <Box display={{ base: 'none', md: 'block' }}>
-      <ButtonGroup variant='ghost' colorScheme='gray'>
-        <Link href='/Invoices'>
-          <Button size='sm'>Invoices</Button>
-        </Link>
-        <Link href='/Validators'>
-          <Button size='sm'>Validators</Button>
-        </Link>
-        <Link href='/Profiles'>
-          <Button size='sm'>Profile</Button>
-        </Link>
-        <Link href='/Rolodex'>
-          <Button size='sm'>Rolodex</Button>
-        </Link>
-        <CustomConnectButton />
-      </ButtonGroup>
-    </Box>
-  </header>
+  {/* Expanded menu for large screens */}
+  <Box display={{ base: 'none', md: 'block' }}>
+    <ButtonGroup variant='ghost' colorScheme='gray'>
+      <Link href='/Invoices'>
+        <Button size='sm'>Invoices</Button>
+      </Link>
+      <Link href='/Validators'>
+        <Button size='sm'>Validators</Button>
+      </Link>
+      <Link href='/Profiles'>
+        <Button size='sm'>Profile</Button>
+      </Link>
+      <Link href='/Rolodex'>
+        <Button size='sm'>Rolodex</Button>
+      </Link>
+      <CustomConnectButton />
+    </ButtonGroup>
+  </Box>
+</header>
 
   );
 };
