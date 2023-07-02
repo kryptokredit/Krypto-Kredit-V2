ab676e94b0a410f16925ccf67de2e664b8d182ee
Elo Mukoro <Elomukoro@gmail.com>
2023-07-02 15:47:04 -0500

    added drawer to header fixed tabs for small screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,83 +1,75 @@
-
- import styles from "@/styles/Header.module.css";
+import styles from "@/styles/Header.module.css";
 import { useState } from 'react';
-import { Box, Button, ButtonGroup, HStack, Spacer, Stack, Text, Link} from "@chakra-ui/react";
+import { Box, Button, VStack, Link, useDisclosure, Drawer, DrawerBody, DrawerOverlay, DrawerContent, HStack, Text, Stack  } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import { CustomConnectButton } from "./CustomConnectButton";
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const handleToggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <header className={styles.navbar}>
-    <div className={styles.brand}>
-      <Link href='/'>
-        <HStack>
-          <img className={styles.logo} src='/logo.png' alt='Krypto Kredit logo' />
-          <Text className={styles.logoText}>Krypto Kredit</Text>
-        </HStack>
-      </Link>
-    </div>
-  
-    {/* Collapsed button for small screens */}
-    <Box display={{ base: 'block', md: 'none' }}>
-      <Button onClick={handleToggleMenu} variant="outline">
-        <HamburgerIcon />
-      </Button>
-   
-{/* 
-    <Button variantColor="blue" onClick={handleToggle}>
-        Toggle
-      </Button> */}
+      <div className={styles.brand}>
+        <Link href='/'>
+          <HStack>
+            <img className={styles.logo} src='/logo.png' alt='Krypto Kredit logo' />
+            <Text className={styles.logoText}>Krypto Kredit</Text>
+          </HStack>
+        </Link>
+      </div>
+
+      {/* Collapsed button for small screens */}
+      <Box display={{ base: 'block', md: 'none' }}>
+        <Button onClick={onOpen} variant="outline">
+          <HamburgerIcon />
+        </Button>
+      </Box>
+
+      {/* Drawer for small screens */}
+      <Drawer placement="right" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay>
+          <DrawerContent>
+            <Box mt="12" flexDirection="column" position="relative">
+              <Stack spacing={3} mt={4} style={{ left: 0 }}>
+                <Link href='/Invoices'>
+                  <Button size='sm' w="full">Invoices</Button>
+                </Link>
+                <Link href='/Validators'>
+                  <Button size='sm' w="full">Validators</Button>
+                </Link>
+                <Link href='/Profiles'>
+                  <Button size='sm' w="full">Profile</Button>
+                </Link>
+                <Link href='/Rolodex'>
+                  <Button size='sm' w="full">Rolodex</Button>
+                </Link>
+                <CustomConnectButton w="full" />
+              </Stack>
+            </Box>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
       
-  
-    {/* Collapsed menu */}
-    <Box mt="12" display={{ base: isMenuOpen ? 'flex' : 'none', md: 'none' }} flexDirection="column" position="relative">
-
-    <Stack spacing={3} mt={4} style={{ left: 0 }}>
-        <Link href='/Invoices'>
-          <Button size='sm' w="full">Invoices</Button>
-        </Link>
-        <Link href='/Validators'>
-          <Button size='sm' w="full">Validators</Button>
-        </Link>
-        <Link href='/Profiles'>
-          <Button size='sm' w="full">Profile</Button>
-        </Link>
-        <Link href='/Rolodex'>
-          <Button size='sm' w="full">Rolodex</Button>
-        </Link>
-        <CustomConnectButton w="full" />
-      </Stack>
-    </Box>
-    </Box>
-  
-    {/* Expanded menu for large screens */}
-    <Box display={{ base: 'none', md: 'block' }}>
-      <Stack direction="row" spacing={3}>
-        <Link href='/Invoices'>
-          <Button size='sm'>Invoices</Button>
-        </Link>
-        <Link href='/Validators'>
-          <Button size='sm'>Validators</Button>
-        </Link>
-        <Link href='/Profiles'>
-          <Button size='sm'>Profile</Button>
-        </Link>
-        <Link href='/Rolodex'>
-          <Button size='sm'>Rolodex</Button>
-        </Link>
-        <CustomConnectButton />
-      </Stack>
-    </Box>
-  </header>
-
+      {/* Expanded menu for large screens */}
+      <Box display={{ base: 'none', md: 'block' }}>
+        <Stack direction="row" spacing={3}>
+          <Link href='/Invoices'>
+            <Button size='sm'>Invoices</Button>
+          </Link>
+          <Link href='/Validators'>
+            <Button size='sm'>Validators</Button>
+          </Link>
+          <Link href='/Profiles'>
+            <Button size='sm'>Profile</Button>
+          </Link>
+          <Link href='/Rolodex'>
+            <Button size='sm'>Rolodex</Button>
+          </Link>
+          <CustomConnectButton />
+        </Stack>
+      </Box>
+    </header>
   );
 };
 
 export default Header;
-
--- src/pages/Invoices/index.jsx
+++ src/pages/Invoices/index.jsx
@@ -1,45 +1,29 @@
-import {
-  Tabs,
-  TabList,
-  TabPanels,
-  Tab,
-  TabPanel,
-  Card,
-  CardHeader,
-  Heading,
-  Button,
-  Link,
-  Spacer,
-  Select,
-  Text,
-} from "@chakra-ui/react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Card, CardHeader, Heading, } from "@chakra-ui/react";
 import useInvoiceList from "@/hooks/useInvoiceList";
-import InvoicerTable from "@/components/InvoicerTable";
+import InvoicerTab from "@/components/invoicerTab";
+import PayerTab from "@/components/payerTab";
 import Head from "next/head";
-import {useState, useEffect} from 'react'
-
-
+import { useState, useEffect } from 'react';
 
 const InvoiceTabs = () => {
   const { data, loading, error } = useInvoiceList();
-  const [showFullButton, setShowFullButton] = useState(true);
   const [isSmallScreen, setIsSmallScreen] = useState(false);
   const [selectedOption, setSelectedOption] = useState('');
+  const [showFullButton, setShowFullButton] = useState(true);
 
   const handleSelectChange = (event) => {
     setSelectedOption(event.target.value);
   };
+
   useEffect(() => {
-    function handleResize() {
-      setIsSmallScreen(window.innerWidth < 768); 
-      setShowFullButton(window.innerWidth > 750); 
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+      setShowFullButton(window.innerWidth > 750);
     }
-
-    // event listener for window resize
+    
     window.addEventListener('resize', handleResize);
-
     handleResize();
-5
+    
     return () => {
       window.removeEventListener('resize', handleResize);
     };
@@ -56,141 +40,38 @@ const InvoiceTabs = () => {
         <link rel='icon' href='/logo.png' />
       </Head>
       <div className='container'>
-        <Card margin={10} maxWidth={1000}  variant='unstyled'>
-          <CardHeader justifyContent='center'></CardHeader>
+        <Card margin={10} maxWidth={1000} variant='unstyled'>
+          <CardHeader justifyContent='center' />
           <Heading size='md' textAlign='center'>
             My Invoices
           </Heading>
           <Tabs variant="soft-rounded" variantColor="green">
-  <TabList>
-    <Tab>Invoicer</Tab>
-    <Tab>Payer</Tab>
-  </TabList>
-  <TabPanels>
-    <TabPanel>
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <Tabs mt={9} variant="enclosed" style={{ width: "80vw" }}>
-<TabList mt={2}>
-      {isSmallScreen ? (
-        
-        <div style={{ width: '50%' }}>
-  <Select  value={selectedOption} onChange={handleSelectChange}>
-    <option value="all">All</option>
-    <option value="unsigned">Unsigned</option>
-    <option value="unpaid">Unpaid</option>
-    <option value="late">Late</option>
-    <option value="paid">Paid</option>
-  </Select>
-</div>
-
-
-      ) : (
-        <>
-          <Tab _hover={{ color: 'black', bg: 'green.400' }}>All</Tab>
-          <Tab _hover={{ color: 'black', bg: 'green.400' }}>Unsigned</Tab>
-          <Tab _hover={{ color: 'black', bg: 'green.400' }}>Unpaid</Tab>
-          <Tab _hover={{ color: 'black', bg: 'green.400' }}>Late</Tab>
-          <Tab _hover={{ color: 'black', bg: 'green.400' }}>Paid</Tab>
-        </>
-      )}
-
-      <Spacer />
-      
-      {/* Rest of your code */}
-  
-        <Link href="/Invoices/InvoiceForm">
-          <Button mb={1} size="md" colorScheme="whatsapp" variant="solid">
-            {showFullButton ? '+ Create an Invoice' : <Text truncate>+</Text>}
-          </Button>
-        </Link>
-     
-    </TabList>
-  <TabPanels style={{width: '80vw'}}>
-    <TabPanel>
-      {loading ? <div>Loading...</div> : <InvoicerTable InvoiceJson={data} />}
-    </TabPanel >
-    <TabPanel>{/* <InvoicerTable InvoiceJson={data} /> */}</TabPanel>
-    <TabPanel>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <InvoicerTable InvoiceJson={data.filter((invoice) => invoice.status === "Unpaid")} />
-      )}
-    </TabPanel>
-    <TabPanel>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <InvoicerTable InvoiceJson={data.filter((invoice) => invoice.status === "Late")} />
-      )}
-    </TabPanel>
-    <TabPanel></TabPanel>
-  </TabPanels>
-</Tabs>
-          </div>
-    </TabPanel>
-    <TabPanel>
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <Tabs mt={9} variant="enclosed" style={{ width: "80vw" }}>
-<TabList mt={2}>
-      {isSmallScreen ? (
-        
-        <div style={{ width: '50%' }}>
-  <Select  value={selectedOption} onChange={handleSelectChange}>
-    <option value="all">All</option>
-    <option value="unsigned">Unsigned</option>
-    <option value="unpaid">Unpaid</option>
-    <option value="late">Late</option>
-    <option value="paid">Paid</option>
-  </Select>
-</div>
-
-
-      ) : (
-        <>
-          <Tab _hover={{ color: 'black', bg: 'green.400' }}>All</Tab>
-          <Tab _hover={{ color: 'black', bg: 'green.400' }}>Unsigned</Tab>
-          <Tab _hover={{ color: 'black', bg: 'green.400' }}>Unpaid</Tab>
-          <Tab _hover={{ color: 'black', bg: 'green.400' }}>Late</Tab>
-          <Tab _hover={{ color: 'black', bg: 'green.400' }}>Paid</Tab>
-        </>
-      )}
-
-      <Spacer />
-      
- 
-  
-
-     
-    </TabList>
-  <TabPanels style={{width: '80vw'}}>
-    <TabPanel>
-      {loading ? <div>Loading...</div> : <InvoicerTable InvoiceJson={data} />}
-    </TabPanel >
-    <TabPanel>{/* <InvoicerTable InvoiceJson={data} /> */}</TabPanel>
-    <TabPanel>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <InvoicerTable InvoiceJson={data.filter((invoice) => invoice.status === "Unpaid")} />
-      )}
-    </TabPanel>
-    <TabPanel>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <InvoicerTable InvoiceJson={data.filter((invoice) => invoice.status === "Late")} />
-      )}
-    </TabPanel>
-    <TabPanel></TabPanel>
-  </TabPanels>
-</Tabs>
-          </div>
-    </TabPanel>
-  </TabPanels>
-</Tabs>
-        
-
+            <TabList>
+              <Tab>Invoicer</Tab>
+              <Tab>Payer</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <InvoicerTab 
+                  isSmallScreen={isSmallScreen}
+                  selectedOption={selectedOption}
+                  handleSelectChange={handleSelectChange}
+                  loading={loading}
+                  data={data}
+                  showFullButton={showFullButton}
+                />
+              </TabPanel>
+              <TabPanel>
+                <PayerTab
+                  isSmallScreen={isSmallScreen}
+                  selectedOption={selectedOption}
+                  handleSelectChange={handleSelectChange}
+                  loading={loading}
+                  data={data}
+                />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </Card>
       </div>
     </>
